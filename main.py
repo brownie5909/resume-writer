40b12b9d3308a783aeb6c8a1819b36beaa4b8e8d
brownie5909 <brownie5909@gmail.com>
2025-08-02 09:23:22 +1000

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ from datetime import datetime, timedelta
 import json
 from routes.interview import router as interview_router
 from routes.resume_analysis import router as resume_analysis_router
+from routes.cover_letter import router as cover_letter_router
+
 
 app = FastAPI()
 
@@ -25,6 +27,7 @@ app.add_middleware(
 
 app.include_router(interview_router)
 app.include_router(resume_analysis_router)
+app.include_router(cover_letter_router)
 
 # Enhanced PDF storage with expiration
 pdf_store = {}
